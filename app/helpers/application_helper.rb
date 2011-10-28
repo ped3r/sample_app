128a6e53100a350e6bb5d6c8280716ce582f4dd1
ped3r <ped3r@hotmail.co.uk>
2011-10-28 15:00:11 +0100

    Added default titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+#return a default title if none is present in page_controller
+	def title
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
+	
 end
